a9e0a451faec347829b66ffb8caa91f135470df3
Michael Lajlev <lajlev@gmail.com>
2019-01-22 14:54:22 +0100

    auto resize iframe script

--- app.js
+++ app.js
@@ -226,3 +226,12 @@ function init(SETTINGS) {
 }
 
 loadJSON("config.json", init);
+
+// Send message to the top window (parent) at 500ms interval
+setInterval(function() {
+  // first parameter is the message to be passed
+  // second paramter is the domain of the parent 
+  // in this case "*" has been used for demo sake (denoting no preference)
+  // in production always pass the target domain for which the message is intended 
+  window.top.postMessage(document.body.scrollHeight, "*");
+}, 500); 
